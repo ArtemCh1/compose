ce2e3f86170dd760cdf294978809083ef3bbea02
ArtemCh1 <xthysqfhntv@mail.ru>
2023-05-03 14:24:24 +0500

    ultra

--- migrations/env.py
+++ migrations/env.py
@@ -2,8 +2,10 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from os import environ
 
 from alembic import context
+from src.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -13,12 +15,12 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
+    
+config.set_main_option('sqlalchemy.url', environ.get('DATABASE_URL'))
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from src.models import Base
 target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
